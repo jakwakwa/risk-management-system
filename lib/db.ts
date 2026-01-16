eed39f15d71b5e326effe7e5fd8c2a03895d1cb5
Jaco Kotzee <jakwakwa@gmail.com>
2026-01-16 22:06:42 +0200

    Update Prisma client import paths

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/client';
 
 const connectionString = process.env.DATABASE_URL;
 
